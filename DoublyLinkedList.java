ca97606e46b3179259bccd150dad9c67ec71f789
shahrukh010 <khanshahrukhbsc@gmail.com>
2021-11-06 15:07:21 +0530

    DoublyLinkedList

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -31,10 +31,38 @@ public class DoublyLinkedList {
         }
     }
 
+    public void add(int index, int data){
+
+            
+          Node newNode = new Node(data);
+
+          if(index==0){
+              newNode.next = this.first;
+              this.first.prev = newNode;
+              this.first = newNode;
+          }
+          else{
+
+              Node tmp = this.first;
+
+              for(int i=0;i<index-1&&tmp !=null;i++){
+                  
+                   tmp = tmp.next;
+              }
+              newNode.next = tmp.next;
+              newNode.prev = tmp;
+
+              if(tmp.next !=null){
+                  tmp.next.prev = newNode;
+              }
+              tmp.next = newNode;
+          }
+    }
+
     public void print(){
 
         Node current = this.first;
-        while(current.next !=null){
+        while(current !=null){
             System.out.print(current.data+" ");
             current = current.next;
         }
@@ -51,6 +79,11 @@ public class DoublyLinkedList {
                          list.add(40);
                          list.add(50);
                          list.add(60);
+
+                         //list.add(0,5);
+                         //list.add(0,2);
+                         list.add(5,55);
+                         list.add(7,70);
 
                          list.print();
 //                         System.out.println(list.last.prev.data);
