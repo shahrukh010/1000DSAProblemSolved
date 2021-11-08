b0d7feb3485c10bdb60afecae4dbd99d186fc679
shahrukh010 <khanshahrukhbsc@gmail.com>
2021-11-08 15:24:54 +0530

    doublylist

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -36,6 +36,10 @@ public class DoublyLinkedList {
             
           Node newNode = new Node(data);
 
+          if(this.first==null){
+              add(data);
+              return;
+          }
           if(index==0){
               newNode.next = this.first;
               this.first.prev = newNode;
@@ -49,6 +53,7 @@ public class DoublyLinkedList {
                   
                    tmp = tmp.next;
               }
+              try{
               newNode.next = tmp.next;
               newNode.prev = tmp;
 
@@ -56,8 +61,63 @@ public class DoublyLinkedList {
                   tmp.next.prev = newNode;
               }
               tmp.next = newNode;
+              }
+              catch(NullPointerException e){
+                  System.out.println("invalid index");
+                  return;
+              }
           }
     }
+
+    public void delete(int index){
+
+        if(index == 0){
+
+            Node tmp = first.next;
+            first = tmp;
+
+            if(first.next !=null){//may be chance of only single node
+                first.prev=  null;
+            }
+        }
+        else{
+            Node tmp = this.first;
+
+            for(int i=0;i<index&& tmp !=null;i++){
+
+                tmp = tmp.next;
+            }
+            try{
+                tmp.prev.next = tmp.next;
+                tmp.prev = null;
+            }
+            catch(NullPointerException e){
+                System.out.println("invalid index");
+                return;
+            }
+        }
+    }
+
+//reversing link
+    public void reverse(){
+
+
+        Node tmp =null;
+        Node current =first;
+
+        while(current!=null){
+            
+            tmp = current.prev;
+            current.prev = current.next;
+            current.next = tmp;
+            current = current.prev;
+        }
+        if(tmp !=null){
+            first = tmp.prev;
+        }
+    }
+                
+            
 
     public void print(){
 
@@ -84,7 +144,14 @@ public class DoublyLinkedList {
                          //list.add(0,2);
                          list.add(5,55);
                          list.add(7,70);
+ //                        list.add(8,170);
 
+                         //list.delete(0);
+//                         list.delete(9);//invalid index
+
+
+                         list.print();
+                         list.reverse();
                          list.print();
 //                         System.out.println(list.last.prev.data);
         }
