f54b80c0e658034460e6540341dc1302c5fb906b
shahrukh010 <khanshahrukhbsc@gmail.com>
2021-11-09 12:09:57 +0530

    searching

--- Searching.java
+++ Searching.java
@@ -3,6 +3,57 @@ public class Searching {
 
 
     
+
+    static java.util.function.BiFunction<int[],Integer,Integer>lastIndexOf =(input,data)->{
+
+                    int start = 0;
+                    int end =input.length-1;
+                    int n = input.length;
+
+                    while(start<=end){
+
+                        int mid = ((start+end)/2);
+                        
+                        if(input[mid] > data){
+                            end = mid-1;
+                        }
+                        else if(input[mid] < data){
+                            start = mid+1;
+                        }
+                        else{
+                            if(mid !=n-1 || input[mid] !=input[mid+1])
+                                return mid;
+                             else
+                                 start = mid+1;
+                        }
+                    }
+                    return -1;
+    };
+    static java.util.function.BiFunction<int[],Integer,Integer>indexOf = (input,data)->{
+
+                    int start = 0;
+                    int end =input.length-1;
+
+                    while(start<=end){
+
+                        int mid = ((start+end)/2);
+
+                        if(input[mid]==data){
+                            if(mid==0|| input[mid-1] !=data)return mid;
+                            else
+                                end = mid-1;
+                                //return;
+                        }
+
+                         if(input[mid] > data){
+                             end = mid-1;
+                         }
+                         else if(input[mid]<data){
+                             start = mid+1;
+                         }
+                    }
+                    return -1;
+    };
     static java.util.function.BiPredicate<int[],Integer> binarySearch = (input,x)->{
 
                 
@@ -18,7 +69,7 @@ public class Searching {
                     else if(input[mid] > x){
                         end = mid-1;
                     }
-                    else if(input[mid] < x){
+                    else{
                         start = mid+1;
                     }
                 }
@@ -27,7 +78,7 @@ public class Searching {
 
     public static void main(String...strings){
                 
-                int[] arr = {-50,-40,10,20,30,40,50,100};
+                int[] arr = {10,10,10,20,30,40,50,50,50,50,100,100,100};
                 
                 //System.out.println(binarySearch.test(arr,20));
                 System.out.println(binarySearch.test(arr,70));
@@ -42,5 +93,12 @@ public class Searching {
                 System.out.println(binarySearch.test(arr,-50));
                 System.out.println(binarySearch.test(arr,-40));
                 System.out.println(binarySearch.test(arr,5000));
+                System.out.println(indexOf.apply(arr,10));
+                System.out.println(indexOf.apply(arr,50));
+                System.out.println(indexOf.apply(arr,-50));
+                System.out.println(lastIndexOf.apply(arr,10));
+                System.out.println(lastIndexOf.apply(arr,40));
+                System.out.println(lastIndexOf.apply(arr,100));
+
     }
 }
