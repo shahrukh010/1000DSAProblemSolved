9a551350fa40afb40edd3dbf28791a568d3562dd
shahrukh010 <khanshahrukhbsc@gmail.com>
2021-11-10 11:38:42 +0530

    fix bugs

---
+++ Execute.java
@@ -0,0 +1,82 @@
+
+public class Execute {
+
+    public static void main(String...strings){
+
+        Arrays array = new Arrays();
+
+        /*
+        array.insert(10);
+        array.insert(30);
+        array.insert(20);
+        array.insert(15);
+        array.insert(20);
+        array.insert(25);
+        */
+
+//        System.out.println(array);
+//        array.leftRotate(array.getArray(),4);
+ //       System.out.println(array);
+
+/*
+        array.add(7);
+        array.add(10);
+        array.add(4);
+        array.add(3);
+        array.add(6);
+        array.add(5);
+        array.add(2);
+*/
+
+/*
+        array.add(30);
+        array.add(10);
+        array.add(8);
+        array.add(2);
+*/
+/*
+        array.add(10);
+        array.add(12);
+        array.add(14);
+        array.add(7);
+        array.add(7);
+        array.add(10);
+        array.add(11);
+*/
+
+
+    //      array.leader(array.getArray());
+//        int result = array.maxDiff.apply(array.getArray());
+ //       System.out.println(result);
+
+ //       array.frequence.accept(array.getArray());
+  //      System.out.println(array.odd_even.apply(array.getArray()));
+
+
+/*
+          array.add(3);
+          array.add(3);
+          array.add(4);
+*/
+//          array.add(8);
+
+ //         System.out.println(array.majorityElement.apply(array.getArray()));
+
+/*
+          array.add(1);
+          array.add(8);
+          array.add(30);
+          array.add(-5);
+          array.add(20);
+          array.add(7);
+*/
+          array.add(5);
+          array.add(-10);
+          array.add(6);
+          array.add(90);
+          array.add(3);
+
+          System.out.println(array.maxKthSum.apply(array.getArray(),2));
+
+    }
+}
---
+++ sum.java
@@ -0,0 +1,13 @@
+class Test{
+
+    public static void main(String[]args)
+    {
+
+        int a=7;
+        int b=8;
+        int c=12;
+
+        int total = a+b+c;
+        System.out.println(total);
+    }
+}
