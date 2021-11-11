5ffcff3de36636a7b9abc20632862ff59e2d3dee
shahrukh010 <khanshahrukhbsc@gmail.com>
2021-11-11 14:10:46 +0530

    commit

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 
+
+
 public class Main {
 
 
@@ -13,7 +15,13 @@ public class Main {
                        list.addNode(30);
                        list.addNode(40);
                        list.addNode(50);
+                       list.addNode(60);
+                       list.addNode(80);
                     System.out.println(list);
+                    System.out.println(list.middle());
+
+//*********************************************************************************************************
+                    /*
                     System.out.println(list.addElement());
                     System.out.println(list.maximum());
 //                    System.out.println(list.search(30));
@@ -30,7 +38,45 @@ public class Main {
                         list.remove(10);
                         list.addNode(0);
                         System.out.println(list.isSorted());
+
                     System.out.println(list);
+                        list.reverse();
+                    System.out.println(list);
+                    */
+
+//*********************************************************************************************************
+
+
+/*
+                    list.add(100);
+                    list.add(200);
+                    list.add(300);
+                    list.add(400);
+                    list.add(500);
+                    list.add(700);
+
+                    list.display();
+                    System.out.println();
+    
+            System.out.println();
+            System.out.println();
+
+            list.merge(list.getFirst(),list.getSecond());
+                    System.out.println(list);
+
+*/
+
+
+        CircularList clist = new CircularList();
+
+                     clist.add(10);
+                     clist.add(20);
+                     clist.add(30);
+                     clist.add(1,21);
+                     clist.add(0,5);
+                     clist.print();
+
+
 
     }
 }
