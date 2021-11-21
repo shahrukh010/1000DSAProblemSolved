8533c6d5bb2d5ef18dac44f50ce52b0f8488d6e1
shahrukh010 <khanshahrukhbsc@gmail.com>
2021-11-21 14:49:36 +0530

    Tree

--- ConstructTree.java
+++ ConstructTree.java
@@ -65,20 +65,43 @@ public class ConstructTree{
         }
 
 
+        public void preOrder(Node node){
+
+            java.util.Stack<Node> stack = new java.util.Stack<>();
+
+            while(node !=null || !stack.isEmpty()){
+
+                if(node !=null){
+                    System.out.println(node.data);
+                    stack.push(node);
+                    node = node.left;
+                }
+                else{
+                    Node tmp = stack.pop();
+                    node = tmp.right;
+                }
+            }
+        }
+
 
     public static void main(String[]args){
 
         
         ConstructTree tree = new ConstructTree();
         ConstructTree.Node node = tree.new Node();
-                           node.insert(10);
-                           node.insert(5);
                            node.insert(8);
-                           node.insert(15);
-                           tree.inOrder(node);
-                           System.out.println(tree.contains(node,15));
-                           System.out.println(tree.contains(node,8));
-                           System.out.println(tree.contains(node,11));
+                           node.insert(3);
+                           node.insert(4);
+                           node.insert(9);
+                           node.insert(5);
+                           node.insert(7);
+                           node.insert(2);
+//                           tree.inOrder(node);
+ //                          System.out.println(tree.contains(node,15));
+  //                         System.out.println(tree.contains(node,8));
+   //                        System.out.println(tree.contains(node,11));
+
+                           tree.preOrder(node);
     }
     
 }
